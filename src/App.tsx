6d1a89de5e745deb0de5a9f46c3795668f2eb769
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-20 10:54:46 +0100

    mixins in

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ function App() {
     <>
     <h1>Sass with @import</h1>
     <p>testing</p>
-    </>
+   
+        <p className="error-text spacing">This is an error.</p>
+        <p className="info-text spacing">This is an info.</p>
+        <p className="normal-text spacing">This is a normal text.</p>
+        <p className="faded-text spacing">This is a faded text.</p>
+         </>
   )
 }
 
