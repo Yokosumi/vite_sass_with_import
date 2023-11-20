a17745bf8e6a0c75f744b799fac2c873d71c7f4f
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-20 11:27:42 +0100

    mixins in

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import './sass/main.scss';
+import "./sass/main.scss";
 
 function App() {
   return (
     <>
-    <h1>Sass with @import</h1>
-    <p>testing</p>
-   
-        <p className="error-text spacing">This is an error.</p>
-        <p className="info-text spacing">This is an info.</p>
-        <p className="normal-text spacing">This is a normal text.</p>
-        <p className="faded-text spacing">This is a faded text.</p>
-         </>
-  )
+      <h1>Sass with @import</h1>
+
+      <p className="error-text spacing">This is an error.</p>
+      <p className="info-text spacing">This is an info.</p>
+      <p className="normal-text spacing">This is a normal text.</p>
+      <p className="faded-text spacing">This is a faded text.</p>
+    </>
+  );
 }
 
-export default App
+export default App;
